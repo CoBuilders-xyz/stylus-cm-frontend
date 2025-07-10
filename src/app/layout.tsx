dfb22181e7ecf51f5088fd97207bc2870d3adab6
Ignacio fq <ignacio@cobuilders.xyz>
2025-07-10 14:44:12 -0300

    Update layout metadata for Stylus Cache Manager
    - Changed the title from 'Arbitrum Cache Manager UI' to 'Stylus Cache Manager' for better alignment with the application's purpose.
    - Updated the description to reflect the focus on managing Arbitrum Stylus Cache, enhancing clarity for users.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ import { Toaster } from '@/components/ui/sonner';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Arbitrum Cache Manager UI',
-  description: 'For Managing Arbitrum Cache',
+  title: 'Stylus Cache Manager',
+  description: 'For Managing Arbitrum Stylus Cache',
 };
 
 export default function RootLayout({
