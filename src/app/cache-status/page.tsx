9e234a4e64444799deb54da1dade78321904bb56
Ignacio fq <ignacio@cobuilders.xyz>
2025-07-31 13:07:29 -0300

    Refactor CacheStatusPage layout and update Footer component margin for better spacing

--- src/app/cache-status/page.tsx
+++ src/app/cache-status/page.tsx
@@ -7,7 +7,7 @@ import CacheManagerActivity from '@/components/CacheManagerActivity';
 export default function CacheStatusPage() {
   return (
     <div
-      className='pt-22 min-h-screen'
+      className='pt-22 min-h-screen flex flex-col'
       style={{
         background:
           'linear-gradient(180deg, #116AAE -193.97%, #072C48 152.16%)',
@@ -16,10 +16,14 @@ export default function CacheStatusPage() {
       <CacheStatus />
 
       {/* Metrics Section */}
-      <div className='w-full p-4 px-10'>
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-          <CacheAverageBid />
-          <CacheManagerActivity />
+      <div className='w-full p-2 px-10 flex-1 mb-3'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 h-full'>
+          <div className='h-full'>
+            <CacheAverageBid />
+          </div>
+          <div className='h-full'>
+            <CacheManagerActivity />
+          </div>
         </div>
       </div>
     </div>
