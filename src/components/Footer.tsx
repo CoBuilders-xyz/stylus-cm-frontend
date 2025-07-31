900a76456dbe2ef48c5fca38ce32df8d9375e731
Ignacio fq <ignacio@cobuilders.xyz>
2025-07-31 12:37:57 -0300

    Update Footer component styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className='py-4 mt-auto'>
+    <footer className='fixed bottom-1 left-0 right-0 z-10 bg-transparent'>
       <div className='container mx-auto px-4 text-center'>
-        <p className='text-sm text-gray-600'>
-          Developed by{' '}
+        <p className='text-sm text-gray-300'>
+          Built by{' '}
           <a
             href='https://www.cobuilders.xyz/'
             target='_blank'
             rel='noopener noreferrer'
-            className='text-blue-600 hover:text-blue-800 transition-colors duration-200'
+            className='text-gray-100 font-bold hover:text-gray-500 transition-colors duration-200'
           >
             CoBuilders.xyz
           </a>
