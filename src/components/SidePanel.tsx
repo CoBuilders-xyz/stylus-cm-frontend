aa8ce4749327fe39d0bcb68ad46ad6500b090477
Augusto Collerone <collerone.augusto@gmail.com>
2026-08-06 14:17:45 +0200

    fix: contain reverse tabbing in side panels

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -111,7 +111,11 @@ const SidePanel: React.FC<SidePanelProps> = ({
 
       const first = focusable[0];
       const last = focusable[focusable.length - 1];
-      if (event.shiftKey && document.activeElement === first) {
+      if (
+        event.shiftKey &&
+        (document.activeElement === panelRef.current ||
+          document.activeElement === first)
+      ) {
         event.preventDefault();
         last.focus();
       } else if (!event.shiftKey && document.activeElement === last) {
